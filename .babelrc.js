74a16671a42b4a24a47fb3e2bbce75ead30b3a33
Dave Willenberg <dave@detroit-english.de>
2018-08-27 22:42:53 +0200

    single-file target + dynamic config w. babelrc.js; exclude dom polyfill

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "modules": false,
+        "loose": true,
+        "useBuiltIns": "usage",
+        "debug": process.env.WORKER_ACTION === 'build',
+        "targets": {
+          "browsers": "last 1 Chrome versions"
+        },
+        "exclude": [
+          "web.dom.iterable" // We ain't got no DOM...
+        ]
+      }
+    ]
+  ],
+  "plugins": []
+}
