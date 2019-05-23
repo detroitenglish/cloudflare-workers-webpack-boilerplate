d204a9fe797aba809315f396887da0f09365ed97
Dave Willenberg <dave@detroit-english.de>
2019-05-23 12:04:25 +0200

    👋 Hello from Workers KV! 👋

--- src/example.worker.js
+++ src/example.worker.js
@@ -1,9 +1,9 @@
 /* eslint-env worker, browser, commonjs */
-/* globals INJECTED_VARIABLE GREETING */
+/* globals INJECTED_VARIABLE GREETING helloNamespace */
 
-// `require()`ed modules and our Worker script are bundled into a single script at build time
-//  Alternatively, you can use import statements, e.g.:
-//    import sample from 'lodash.sample'
+// `require()`ed modules and our Worker script are bundled into a single script
+//  at build time Alternatively, you can use import statements, e.g.: import
+//  sample from 'lodash.sample'
 const sample = require(`lodash.sample`)
 
 // This is our worker's listener function
@@ -17,7 +17,7 @@ async function requestHandler(request) {
   const response = await fetch(request)
 
   // A random 'hello' from out helper function
-  const hello = utilityFunction()
+  const hello = await utilityFunction()
   const greeting = `${hello}, world!`
 
   // Create a new Headers object for our modified response
@@ -37,8 +37,8 @@ async function requestHandler(request) {
   return new Response(response.body, modifiedResponseOptions)
 }
 
-// This is a simple helper function for our requestHandler
-function utilityFunction() {
+// A simple helper function for the requestHandler
+async function utilityFunction() {
   const hellos = [
     'Hello',
     'Konnichi wa',
@@ -54,15 +54,38 @@ function utilityFunction() {
     'Hola',
     'God dag',
     'Xin chao',
-    // Below is a secret variable GREETING found in src/example.metadata.json (default: 'Howdy')
-    // The 'secret' is stored in the Cloudflare Worker Secrets Vault and automatically
-    // injected when the script is executed.
-    GREETING,
-    // Below is EXAMPLE_GREETING found in the .env config file (default: 'Guten Tag')
-    // It's passed via webpack.config.js to Webpack, which then injects the greeting upon build
-    INJECTED_VARIABLE,
   ]
 
+  try {
+    if (INJECTED_VARIABLE) {
+      /*
+        Below is EXAMPLE_GREETING found in the .env config file (default: 'Guten
+        Tag') It's passed via webpack.config.js to Webpack, which then injects
+        the greeting upon build
+      */
+      hellos.push(INJECTED_VARIABLE)
+    }
+
+    if (GREETING) {
+      /*
+        Below is the secret variable GREETING found in src/example.metadata.json
+        (default: 'Howdy') The 'secret' is stored in the Cloudflare Worker
+        Secrets Vault and automatically injected when the script is executed.
+      */
+      hellos.push(GREETING)
+    }
+
+    if (helloNamespace) {
+      /*
+        Below is the kv_greeting (default: 'Miten menee') found in our
+        helloNamespace namespace, which we bind to the worker via
+        example.metadata.json. The KV
+      */
+      hellos.push(await helloNamespace.get(`kv_greeting`))
+    }
+  } catch (err) {
+    console.error(err)
+  }
   // Here, we use the lodash.sample module that we 'required' above
   return sample(hellos)
 }
